ca51e24d2298b619ee3439d81addf49f9ff03b92
Gamze Güneri <62291935+pinacotheque@users.noreply.github.com>
2021-05-26 18:03:08 +0300

    after rabia helped us

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 let products = []
 let shopItems = document.getElementById("row")
 
@@ -14,50 +16,39 @@ window.onload = () => {
 
 
     })
-    .catch((err)=> console.log(err))
    
 }
-const globalData = [];
+const url = "https://striveschool-api.herokuapp.com/api/product/";
 
       window.onload = async function () {
         const url = "https://striveschool-api.herokuapp.com/api/agenda/";
         const currentEvents = document.getElementById("currentEvents");
 
-        // fetch(url, { method: "GET" })
-        //   .then((res) => res.json())
-        //   .then((events) => {
-        //       DOM MANIPULATION GOES HERE
-        //       forEach
-        //   })
-        //   .catch((err) => console.log(err));
-
         try {
-          const response = await fetch(url); // this is getting the response from the API for a fetch request
-          const events = await response.json(); // this is transforming the response in a json, asyncronous operation!
+          const response = await fetch(url,{
+              'method': 'GET',
+              'headers': {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGFlNDE5ZWNlYWY0ODAwMTVjOTE4NzIiLCJpYXQiOjE2MjIwMzI3OTgsImV4cCI6MTYyMzI0MjM5OH0.DgYZBl7qEA5Bp4LmgBUEcFVUWLySQfd-KQksu872Z2o",
+            }
+          })
+          
+          const products = await response.json(); 
+        console.log(products)
+        products.forEach((product,i)=>{
+            shopItems +=` <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+             <p class="card-text">${product.price}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>`
+        })
+          
 
-          //only after "response" and "events" variables receive the data, they get created, and the execution of the following code resumes
-          //you can be sure that "events" variable will always have a value inside, and will never be undefined
-          if (events.length > 0) {
-            events.forEach((event) => {
-              const listItem = document.createElement("li");
-              listItem.classList.add(
-                "list-group-item",
-                "d-flex",
-                "justify-content-between"
-              );
-
-              listItem.innerHTML = `<span>${event.name}</span><span class="badge badge-dark">${event.price}€</span>`;
-
-              currentEvents.appendChild(listItem);
-            });
-          } else {
-            currentEvents.innerHTML = `<div class="alert alert-info" role="alert">
-                                        No events planned yet!
-                                        </div>`;
-          }
-
-        } catch (err) {
+         
+      } catch (err) {
           console.log(err);
         }
-        console.log("this is happening after everything");
-      };
+    }
